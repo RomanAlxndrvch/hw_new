29275d29a7292f9ea0356faacc7c78a427c6183a
Roman Troian <89692499+RomanAlxndrvch@users.noreply.github.com>
2022-10-09 16:27:48 -0600

    hw5

--- src/s2-homeworks/hw05/layout/Layout.tsx
+++ src/s2-homeworks/hw05/layout/Layout.tsx
@@ -1,25 +1,25 @@
-import React, { FC, ReactNode, useEffect, useState } from 'react'
-import { Header } from '../header/Header'
-import { Sidebar } from '../sidebar/Sidebar'
+import React, {FC, ReactNode, useEffect, useState} from 'react'
+import {Header} from '../header/Header'
+import {Sidebar} from '../sidebar/Sidebar'
 
 type PropsType = {
     children: ReactNode
 }
 
-export const Layout: FC<PropsType> = ({ children }) => {
-    const [open, setOpen] = useState(false)
+export const Layout: FC<PropsType> = ({children}) => {
+    const [open, setOpen] = useState(true)
     const handleClose = () => setOpen(false)
     const handleOpen = () => setOpen(true)
 
-    useEffect(() => {
+    /*useEffect(() => {
         open && (document.body.style.overflow = 'hidden')
         !open && (document.body.style.overflow = 'unset')
-    }, [open]) // отключает прокрутку при открытом меню
+    }, [open]) */// отключает прокрутку при открытом меню
 
     return (
         <>
-            <Sidebar open={open} handleClose={handleClose} />
-            <Header handleOpen={handleOpen} />
+            <Sidebar open={open} handleClose={handleClose}/>
+            <Header handleOpen={handleOpen}/>
             <div>
                 {/*страницы*/}
                 {children}
