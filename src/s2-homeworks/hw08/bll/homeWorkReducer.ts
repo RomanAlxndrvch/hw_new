4a4f703e7547f204f0f0f4ab6effdfdd6c1a65ab
Roman Troian <89692499+RomanAlxndrvch@users.noreply.github.com>
2022-10-17 22:50:40 -0600

    hw8. not done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,42 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            if (action.payload === 'up') {
+                return [...state.sort(function (a, b) {
+                    if (a.name > b.name) {
+                        return 1
+                    }
+                    else if (a.name < b.name) {
+                        return -1
+                    }
+                    else {
+                        return 0
+                    }
+                })]
+            }
+            if (action.payload === 'down') {
+                return [...state.sort(function (a, b) {
+                    if (a.name > b.name) {
+                        return -1
+                    }
+                    else if (a.name < b.name) {
+                        return 1
+                    }
+                    else {
+                        return 0
+                    }
+                })]
+            }
+            else return state
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter((el) => el.age >= action.payload)
         }
         default:
             return state
+
     }
 }
